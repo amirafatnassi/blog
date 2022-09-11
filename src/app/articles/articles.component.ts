25d9f4ab4c66141d6891d0a4a3fe344c0258c2e0
amirafatnassi <amirafatnassi88@gmail.com>
2022-09-11 22:42:28 +0100

    debug incomplete scully error

--- src/app/articles/articles.component.ts
+++ src/app/articles/articles.component.ts
@@ -1,16 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-import {ScullyRoute, ScullyRoutesService} from '@scullyio/ng-lib';
+import { ScullyRoute, ScullyRoutesService } from '@scullyio/ng-lib';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-articles',
   templateUrl: './articles.component.html',
-  styleUrls: ['./articles.component.scss']
+  styleUrls: ['./articles.component.scss'],
 })
 export class ArticlesComponent implements OnInit {
+  posts: ScullyRoute[] = [];
+  private routeSub: Subscription | undefined;
+  
+  constructor(private scullyService: ScullyRoutesService) {}
 
-  constructor() { }
 
   ngOnInit(): void {
+    this.scullyService.available$.subscribe((posts) => {
+      this.posts = posts.filter((post) => post.title);
+    });
   }
 
+  ngOnDestroy():void{
+    this.routeSub?.unsubscribe();
+  }
 }
